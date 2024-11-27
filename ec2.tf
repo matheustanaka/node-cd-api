3957f8dc062ed12981aefefcd0540d7398592686
Matheus Tanaka <matheustanakanog@gmail.com>
2024-11-27 11:53:21 -0300

    add my-key to ec2

--- ec2.tf
+++ ec2.tf
@@ -23,7 +23,7 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "my_ec2" {
   ami                         = data.aws_ami.ubuntu.id
   instance_type               = "t3.micro"
-  key_name                    = aws_key_pair.key.key_name
+  key_name                    = aws_key_pair.my-key.key_name
   vpc_security_group_ids      = [aws_security_group.allow_ssh.id]
   subnet_id                   = aws_subnet.subnet_ec2_public_az_a.id
   associate_public_ip_address = true
